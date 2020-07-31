991df517e5a2b9df76b2a6c1496b671082f1b220
Hleb Albau <hleb.albau@gmail.com>
2020-07-31 13:15:19 +0300

    Upload archive on test pypi index

--- example/example.py
+++ example/example.py
@@ -5,25 +5,16 @@ from regula.documentreader.webclient.ext.models import RecognitionImage, Recogni
 from regula.documentreader.webclient.gen import ProcessParams, Scenario, Result, Light, TextFieldType, Source, \
     CheckResult
 
-print("START")
+host = os.getenv("API_BASE_PATH", "http://localhost:8080")
+license = os.getenv("TEST_LICENSE", None)
 
-API_BASE_PATH = "API_BASE_PATH"
-host = os.getenv(API_BASE_PATH, None)
-if not host:
-    host = "http://localhost:8080"
-
-TEST_LICENSE = "TEST_LICENSE"
-license = os.getenv(TEST_LICENSE, None)
-
-IMAGE_PATH = "resources/australia_passport.jpg"
-with open(IMAGE_PATH, "rb") as f:
+with open("australia_passport.jpg", "rb") as f:
     image_payload = f.read()
 
-print(host)
 with DocumentReaderApi(host) as api:
     api.license = license
 
-    process_params = ProcessParams(Scenario.FULL_PROCESS, [Result.TEXT, Result.STATUS, Result.IMAGES])
+    process_params = ProcessParams(Scenario.FULL_PROCESS, [Result.STATUS, Result.TEXT, Result.IMAGES])
     process_images = [RecognitionImage(image_payload, Light.WHITE)]
     request = RecognitionRequest(process_params, process_images)
 
@@ -57,4 +48,3 @@ with DocumentReaderApi(host) as api:
               MRZ-Visual values comparison: {doc_number_mrz_visual_matching}
     ---------------------------------------------------------------------------
     """)
-    print("OK")
