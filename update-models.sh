effed5a5a8974ab278df4e3e3a7d7efff369fc55
Aleksey Shundrik <al.shundrik@insspring.com>
2020-11-05 14:45:26 +0300

    rl-492/openapi-generator OpenapiGenerator: - add DOCS_DEFINITION_FOLDER variable into update-models.sh

--- update-models.sh
+++ update-models.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "${PWD}/../DocumentReader-web-openapi:/definitions" \
+DOCS_DEFINITION_FOLDER="${PWD}/../DocumentReader-web-openapi" \
+\
+&& docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "${DOCS_DEFINITION_FOLDER}:/definitions" \
 openapitools/openapi-generator-cli:v5.0.0-beta generate -g python \
 -i /definitions/index.yml -o /client -c /client/generator-config.json \
 -t /client/generator-templates
