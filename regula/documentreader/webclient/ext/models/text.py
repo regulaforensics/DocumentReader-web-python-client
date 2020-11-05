c53f5da37767a5a07c0e95a5e96317eb95fe4b81
Aleksey Shundrik <al.shundrik@insspring.com>
2020-11-05 19:48:28 +0300

    rl-476/field-name FieldName: - add text getters for field property: by type and by name

--- regula/documentreader/webclient/ext/models/text.py
+++ regula/documentreader/webclient/ext/models/text.py
@@ -12,6 +12,24 @@ class Text(GenText):
                 return field
         return None
 
+    def get_field_value(self, field_type: int, lcid: int = None) -> Optional[str]:
+        field = self.get_field(field_type, lcid)
+        if field:
+            return field.value
+        return None
+
+    def get_field_by_name(self, field_name: str, lcid: int = None) -> Optional[TextField]:
+        for field in self.field_list:
+            if field.field_name == field_name and (not lcid or field.lcid == lcid):
+                return field
+        return None
+
+    def get_field_value_by_name(self, field_name: str, lcid: int = None) -> Optional[str]:
+        field = self.get_field_by_name(field_name, lcid)
+        if field:
+            return field.value
+        return None
+
     @GenText.field_list.getter
     def field_list(self) -> List[TextField]:
         # fix type hinting
