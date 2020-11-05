325e9325734f5e8542775e9d733dce3d34c69e99
Aleksey Shundrik <al.shundrik@insspring.com>
2020-11-05 14:37:50 +0300

    rl-492/openapi-generator OpenapiGenerator: - add ability to run v5.0.0-beta openapi generator version - add ability to run update-models.sh without DOCS_DEFINITION_FOLDER environment

--- update-models.sh
+++ update-models.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "${DOCS_DEFINITION_FOLDER}:/definitions" \
-openapitools/openapi-generator-cli generate -g python \
+docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "${PWD}/../DocumentReader-web-openapi:/definitions" \
+openapitools/openapi-generator-cli:v5.0.0-beta generate -g python \
 -i /definitions/index.yml -o /client -c /client/generator-config.json \
 -t /client/generator-templates
