b3507a56fe6f434c853d326d763f282688d4f4b1
Hleb Albau <hleb.albau@gmail.com>
2020-11-06 23:10:31 +0300

    more advanced field extractor

--- regula/documentreader/webclient/ext/models/text.py
+++ regula/documentreader/webclient/ext/models/text.py
@@ -1,34 +1,42 @@
 from typing import Optional, List
 
 from regula.documentreader.webclient.ext.models.text_field import TextField
-from regula.documentreader.webclient.gen.models import Text as GenText
+from regula.documentreader.webclient.gen.models import LCID, Text as GenText
 
 
 class Text(GenText):
 
     def get_field(self, field_type: int, lcid: int = None) -> Optional[TextField]:
+        result = None
         for field in self.field_list:
-            if field.field_type == field_type and (not lcid or field.lcid == lcid):
-                return field
-        return None
+            if field.field_type == field_type:
+                if lcid is not None and field.lcid == lcid:
+                    return field
+                elif lcid is None and field.lcid == LCID.LATIN:
+                    return field
+                elif lcid is None and result is None:
+                    result = field
+        return result
 
     def get_field_value(self, field_type: int, lcid: int = None) -> Optional[str]:
         field = self.get_field(field_type, lcid)
-        if field:
-            return field.value
-        return None
+        return field.value if field else None
 
     def get_field_by_name(self, field_name: str, lcid: int = None) -> Optional[TextField]:
+        result = None
         for field in self.field_list:
-            if field.field_name == field_name and (not lcid or field.lcid == lcid):
-                return field
-        return None
+            if field.field_name == field_name:
+                if lcid is not None and field.lcid == lcid:
+                    return field
+                elif lcid is None and field.lcid == LCID.LATIN:
+                    return field
+                elif lcid is None and result is None:
+                    result = field
+        return result
 
     def get_field_value_by_name(self, field_name: str, lcid: int = None) -> Optional[str]:
         field = self.get_field_by_name(field_name, lcid)
-        if field:
-            return field.value
-        return None
+        return field.value if field else None
 
     @GenText.field_list.getter
     def field_list(self) -> List[TextField]:
