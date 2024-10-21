098cfb9451f37a2f8b117bb0e5078a76f08e29d0
GitHub Action <action@github.com>
2024-10-21 14:35:01 +0000

    TESTING UPDATED OPEN API

--- regula/documentreader/webclient/gen/models/process_params.py
+++ regula/documentreader/webclient/gen/models/process_params.py
@@ -70,6 +70,7 @@ class ProcessParams(object):
         'return_uncropped_image': 'bool',
         'mrz_formats_filter': 'list[MRZFormat]',
         'force_read_mrz_before_locate': 'bool',
+        'force_barcode_checks': 'bool',
         'parse_barcodes': 'bool',
         'convert_case': 'TextPostProcessing',
         'split_names': 'bool',
@@ -127,6 +128,7 @@ class ProcessParams(object):
         'return_uncropped_image': 'returnUncroppedImage',
         'mrz_formats_filter': 'mrzFormatsFilter',
         'force_read_mrz_before_locate': 'forceReadMrzBeforeLocate',
+        'force_barcode_checks': 'forceBarcodeChecks',
         'parse_barcodes': 'parseBarcodes',
         'convert_case': 'convertCase',
         'split_names': 'splitNames',
@@ -145,7 +147,7 @@ class ProcessParams(object):
         'generate_numeric_codes': 'generateNumericCodes'
     }
 
-    def __init__(self, lcid_filter=None, lcid_ignore_filter=None, one_shot_identification=None, use_face_api=None, face_api=None, do_detect_can=None, image_output_max_height=None, image_output_max_width=None, scenario=None, result_type_output=None, double_page_spread=None, generate_double_page_spread_image=None, field_types_filter=None, date_format=None, measure_system=None, image_dpi_out_max=None, already_cropped=None, custom_params=None, config=None, log=None, log_level=None, force_doc_id=None, match_text_field_mask=None, fast_doc_detect=None, update_ocr_validity_by_glare=None, check_required_text_fields=None, return_cropped_barcode=None, image_qa=None, respect_image_quality=None, force_doc_format=None, no_graphics=None, depersonalize_log=None, multi_doc_on_image=None, shift_expiry_date=None, minimal_holder_age=None, return_uncropped_image=None, mrz_formats_filter=None, force_read_mrz_before_locate=None, parse_barcodes=None, convert_case=None, split_names=None, disable_perforation_ocr=None, document_group_filter=None, process_auth=None, device_id=None, device_type=None, device_type_hex=None, ignore_device_id_from_image=None, document_id_list=None, rfid=None, check_auth=None, auth_params=None, mrz_detect_mode=None, generate_numeric_codes=None, local_vars_configuration=None):  # noqa: E501
+    def __init__(self, lcid_filter=None, lcid_ignore_filter=None, one_shot_identification=None, use_face_api=None, face_api=None, do_detect_can=None, image_output_max_height=None, image_output_max_width=None, scenario=None, result_type_output=None, double_page_spread=None, generate_double_page_spread_image=None, field_types_filter=None, date_format=None, measure_system=None, image_dpi_out_max=None, already_cropped=None, custom_params=None, config=None, log=None, log_level=None, force_doc_id=None, match_text_field_mask=None, fast_doc_detect=None, update_ocr_validity_by_glare=None, check_required_text_fields=None, return_cropped_barcode=None, image_qa=None, respect_image_quality=None, force_doc_format=None, no_graphics=None, depersonalize_log=None, multi_doc_on_image=None, shift_expiry_date=None, minimal_holder_age=None, return_uncropped_image=None, mrz_formats_filter=None, force_read_mrz_before_locate=None, force_barcode_checks=None, parse_barcodes=None, convert_case=None, split_names=None, disable_perforation_ocr=None, document_group_filter=None, process_auth=None, device_id=None, device_type=None, device_type_hex=None, ignore_device_id_from_image=None, document_id_list=None, rfid=None, check_auth=None, auth_params=None, mrz_detect_mode=None, generate_numeric_codes=None, local_vars_configuration=None):  # noqa: E501
         """ProcessParams - a model defined in OpenAPI"""  # noqa: E501
         if local_vars_configuration is None:
             local_vars_configuration = Configuration()
@@ -189,6 +191,7 @@ class ProcessParams(object):
         self._return_uncropped_image = None
         self._mrz_formats_filter = None
         self._force_read_mrz_before_locate = None
+        self._force_barcode_checks = None
         self._parse_barcodes = None
         self._convert_case = None
         self._split_names = None
@@ -282,6 +285,8 @@ class ProcessParams(object):
             self.mrz_formats_filter = mrz_formats_filter
         if force_read_mrz_before_locate is not None:
             self.force_read_mrz_before_locate = force_read_mrz_before_locate
+        if force_barcode_checks is not None:
+            self.force_barcode_checks = force_barcode_checks
         if parse_barcodes is not None:
             self.parse_barcodes = parse_barcodes
         if convert_case is not None:
@@ -1178,6 +1183,29 @@ class ProcessParams(object):
         """
 
         self._force_read_mrz_before_locate = force_read_mrz_before_locate
+
+    @property
+    def force_barcode_checks(self):
+        """Gets the force_barcode_checks of this ProcessParams.  # noqa: E501
+
+        Allows you to bypass the minimum conditions that should be met to perform the barcode format check  # noqa: E501
+
+        :return: The force_barcode_checks of this ProcessParams.  # noqa: E501
+        :rtype: bool
+        """
+        return self._force_barcode_checks
+
+    @force_barcode_checks.setter
+    def force_barcode_checks(self, force_barcode_checks):
+        """Sets the force_barcode_checks of this ProcessParams.
+
+        Allows you to bypass the minimum conditions that should be met to perform the barcode format check  # noqa: E501
+
+        :param force_barcode_checks: The force_barcode_checks of this ProcessParams.  # noqa: E501
+        :type force_barcode_checks: bool
+        """
+
+        self._force_barcode_checks = force_barcode_checks
 
     @property
     def parse_barcodes(self):
