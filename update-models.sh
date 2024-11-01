d00977911f8ee70d0131af2f38878bec5d3fc8e3
Kiryl Kovaliov <kkirusha@gmail.com>
2024-11-01 18:35:52 +0100

    SP-19086 add custom headers example

--- update-models.sh
+++ update-models.sh
@@ -5,4 +5,9 @@ DOCS_DEFINITION_FOLDER="${PWD}/../DocumentReader-web-openapi" \
 && docker run --user "$(id -u):$(id -g)" --rm -v "${PWD}:/client" -v "$DOCS_DEFINITION_FOLDER:/definitions" \
 openapitools/openapi-generator-cli:v5.0.0 generate -g python-legacy \
 -i /definitions/index.yml -o /client -c /client/generator-config.json \
--t /client/generator-templates
+-t /client/generator-templates \
+--type-mappings AnyOfDocVisualExtendedFieldRectDocVisualExtendedFieldRfid=DocVisualExtendedField \
+--type-mappings AnyOfSecurityFeatureResultIdentResultFiberResultOCRSecurityTextResultPhotoIdentResult=AuthenticityCheckResultItem \
+--type-mappings  AnyOfGraphicFieldRectGraphicFieldRfid=GraphicField \
+--type-mappings AnyOfStatusResultTextResultDocumentImageResultImagesResultChosenDocumentTypeResultDocumentTypesCandidatesResultTextDataResultGraphicsResultLexicalAnalysisResultAuthenticityResultImageQualityResultDocumentPositionResultDocBarCodeInfoLicenseResultEncryptedRCLResultDocumentBinaryInfoResult=ResultItem \
+--type-mappings AnyOfStatusResultTextResultDocumentImageResultImagesResultChosenDocumentTypeResultDocumentTypesCandidatesResultTextDataResultGraphicsResultLexicalAnalysisResultAuthenticityResultImageQualityResultDocumentPositionResultDocBarCodeInfoLicenseResultEncryptedRCLResult=ResultItem
